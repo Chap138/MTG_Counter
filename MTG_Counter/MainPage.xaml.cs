3011c19ee2c13e1111345881dc48d7400a36f6e6
Jason Chapman <jchap71@gmail.com>
2022-09-14 13:57:14 -0700

    Improved layout

--- MTG_Counter/MainPage.xaml.cs
+++ MTG_Counter/MainPage.xaml.cs
@@ -14,5 +14,10 @@ namespace MTG_Counter
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
