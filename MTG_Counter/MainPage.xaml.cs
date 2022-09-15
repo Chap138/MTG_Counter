dc002c0324d23104f216a3f3092154a5efd006d2
Jason Chapman <jchap71@gmail.com>
2022-09-14 21:18:19 -0700

    Buttons change the first four counters

--- MTG_Counter/MainPage.xaml.cs
+++ MTG_Counter/MainPage.xaml.cs
@@ -10,14 +10,57 @@ namespace MTG_Counter
 {
     public partial class MainPage : ContentPage
     {
+        Player player;
         public MainPage()
         {
             InitializeComponent();
+            player = new Player(); ;
+            BindingContext = player;
         }
 
         private void SettingsBtn_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new SettingsPage());
+        }
+
+        private void LifeMinusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.Life -= 1;
+        }
+
+        private void LifePlusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.Life += 1;
+        }
+
+        private void CommTaxMinusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.CommTax -= 1;
+        }
+
+        private void CommTaxPlusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.CommTax += 1;
+        }
+
+        private void ExpMinusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.ExpCtr -= 1;
+        }
+
+        private void ExpPlusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.ExpCtr += 1;
+        }
+
+        private void PoisonMinusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.PoisonCtr -= 1;
+        }
+
+        private void PoisonPlusBtn_Clicked(object sender, EventArgs e)
+        {
+            player.PoisonCtr += 1;
         }
     }
 }
